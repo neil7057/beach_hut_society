ec8a961400e8b173e18c41daaf0d49a24599ff58
Neil Allen <neil7057@gmail.com>
2024-11-06 17:40:15 +0000

    add route for contact page and links

--- run.py
+++ run.py
@@ -1,23 +1,27 @@
 import os
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
-
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title="Latest Forum Posts")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About B.H.A.S")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html", page_title="Contact")
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+    return render_template("contact.html", page_title="Contact Admin")
 
 
 if __name__ == "__main__":
